7b82494f587704b0e56688be888c2301ad620f48
Mehmet Emin Çatal <mhmtmnctl20@gmail.com>
2022-12-27 09:48:35 +0300

    emin

--- src/day06_concatination/C02_Concatenation.java
+++ src/day06_concatination/C02_Concatenation.java
@@ -9,11 +9,11 @@ public class C02_Concatenation {
         int sayi2= 4;
         //Yukarıdaki variableları kullanarak istenilen metinleri yazdıralım
 
-        System.out.println(str1 + " "+ str2+ " "+sayi1+sayi2);//Java Güzeldir 54 yapalım
-        System.out.println(str1+ " "+ str2+ " "+ (sayi1+sayi2));//Jacva güzeldir 9 yazdıralım
+        System.out.println(str1 + " "+ str2+ " "+sayi1+sayi2);//Java Güzeldir 54
+        System.out.println(str1+ " "+ str2+ " "+ (sayi1+sayi2));//Jacva güzeldir 9
         System.out.println(sayi1+sayi2+" "+ str1);//9 Java yazdıralım
         System.out.println(""+sayi1+sayi2+" "+str2);//54 Guzeldir yapalım  "" hiçlik denir string olması için ekledik
-        str1.concat(str2);//javaGuzeldir yapar
+        str1.concat(str2);//javaGuzeldir
         System.out.println( str1.concat(str2));//JavaGuzeldir
         System.out.println(str1.concat(" ").concat(str2));//Java Guzeldir
 
